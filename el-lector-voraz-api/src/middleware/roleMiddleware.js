141a43d622e9d8549af35038c3fae1ab852b21b3
Agustina Bartolomé <agustina.bartolome11@gmail.com>
2025-11-16 20:07:43 -0300

    add authorization and role

---
+++ el-lector-voraz-api/src/middleware/roleMiddleware.js
@@ -0,0 +1,8 @@
+export function permitirRoles(...rolesPermitidos) {
+  return (req, res, next) => {
+    if (!rolesPermitidos.includes(req.user.rol)) {
+      return res.status(403).json({ error: "No tienes permisos" });
+    }
+    next();
+  };
+}
